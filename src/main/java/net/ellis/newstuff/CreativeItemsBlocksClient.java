cc643797c45ff8650938023bbc75d107f15bca35
Idah Matérn <idahmatern@Ellis-Mac-os.local>
2024-11-23 03:39:58 +0100

    Initial commit

---
+++ src/main/java/net/ellis/newstuff/CreativeItemsBlocksClient.java
@@ -0,0 +1,10 @@
+package net.ellis.newstuff;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CreativeItemsBlocksClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        
+    }
+}
---
+++ src/main/java/net/ellis/newstuff/CreativeItemsBlocksDataGenerator.java
@@ -0,0 +1,11 @@
+package net.ellis.newstuff;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CreativeItemsBlocksDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
